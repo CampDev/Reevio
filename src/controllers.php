2193de994897a8ec789bf83bfe788a781269572f
Cacauu <achternbosch@me.com>
2013-09-01 14:49:00 +0200

    Single Post view, small clean up

--- src/controllers.php
+++ src/controllers.php
@@ -17,26 +17,16 @@ function request_posts($url) {
 }
  
 $app->get('/', function () use ($app) {
-	$log = fopen('request.txt', 'a');
-	$entity_uri = $app['reevio.config']['entity_uri'];
-	$entity_sub = substr($entity_uri, 0, strlen($entity_uri)-1);
-    $header_result = get_headers($entity_uri);
-    $discovery_link = str_replace("<", "", $header_result[13]);
-    $discovery_link = str_replace(">", "", $discovery_link);
-    $discovery_link = str_replace("Link: ", "", $discovery_link);
-    $discovery_link = str_replace('; rel="https://tent.io/rels/meta-post"', "", $discovery_link);
+    $meta =discover_link($app['reevio.config']['entity_uri']);
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $entity_sub.$discovery_link);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_VERBOSE, 1);
-    curl_setopt($ch, CURLOPT_STDERR, $log);
     $meta = json_decode(curl_exec($ch));
     curl_close($ch);
 	$meta = discover_link($app['reevio.config']['entity_uri']);
 	$posts_feed = $meta->post->content->servers[0]->urls->posts_feed;
 	$req_url = $posts_feed.'?types=https%3A%2F%2Ftent.io%2Ftypes%2Fessay%2Fv0&limit='.$app['reevio.config']['displayed_essays'];
 	$post = request_posts($req_url);
- 	fclose($log);
 
 	return $app['twig']->render('index.twig', array(
         'posts' => $post->posts,
@@ -45,8 +35,12 @@ $app->get('/', function () use ($app) {
  
 //Single-Post
 $app->get('/post/{id}', function ($id) use ($app) {
+	$meta =discover_link($app['reevio.config']['entity_uri']);
 	$entity_url = urlencode(substr($app['reevio.config']['entity_uri'], 0, strlen($app['reevio.config']['entity_uri'])-1));
-	$req_url = 'https://cacauu.cupcake.is/posts/'.$entity_url.'/'.$id;
+	$single_post = $meta->post->content->servers[0]->urls->post;
+	$single_post = str_replace("{entity}", $entity_url, $single_post);
+	$single_post = str_replace("{post}", $id, $single_post);
+	$req_url = $single_post;
 	$post = request_posts($req_url);
 	return $app['twig']->render('post.twig', array(
         'post' => $post->post,
