4b9ac1bce46ec2c13f9e46c5fa567c4f7da54c7b
Cacauu <achternbosch@me.com>
2014-06-12 21:11:29 +0200

    Remove another log

--- src/controllers.php
+++ src/controllers.php
@@ -4,15 +4,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
  
 function request_posts($url) {
-	$log = fopen('request_post_log.txt', 'w');
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_VERBOSE, 1);
-	curl_setopt($ch, CURLOPT_STDERR, $log);
 	$post = json_decode(curl_exec($ch));
 	curl_close($ch);
-	fclose($log);
 	return $post;
 }
  
